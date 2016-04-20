4fa5390561acef43c56f8d81fd3a3c5546eae39c
brandontaylor1994 <brandontaylor1994@gmail.com>
2016-04-20 15:51:07 -0700

    feature-1: Added comments to code to aid readability

--- server/cardreader/forms.py
+++ server/cardreader/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 
+# Creates a new form with the following field and attributes.
+# Each time the class is instantiated it constructs a new html <form>
 class NameForm(forms.Form):
-    w_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'W########', 'autofocus': 'autofocus'}), label='W#   ', max_length=100)
+    w_number = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'W########', 'autofocus': 'autofocus'}), label='W#   ', max_length=100)
 
--- server/cardreader/views.py
+++ server/cardreader/views.py
@@ -9,18 +9,22 @@ from django.contrib import messages
 def index(request):
     name = "default"
     if request.method == 'POST':
-
+        # Create a new form
         form = NameForm()
 
+        # Use regex to parse out w_number
         name = request.POST['w_number']
-        pattern = re.compile("(W[0-9]{8})")
+        pattern = re.compile("(W[0-9]{8})")  # Could take 'W' out of parenthesis to only capture 8 digits
         match = pattern.search(name)
         name = match.group(1)
 
-        print('Just set name: ' + match.group(1))
+        # Compile message to be sent to web page
         message = "Thank you, " + name
         messages.success(request, message)
+
+        # Redirect the web page to the same path but with the form now empty POST -> GET (avoid resubmission scenario)
         return HttpResponseRedirect(request.path)
     else:
+        # Create a new form and pass it to the web page
         form = NameForm()
     return render(request, 'cardreader/index.html', {'form': form, 'name': name})
