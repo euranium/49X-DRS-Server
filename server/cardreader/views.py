fa2fa3fdae90ad6a6e9a068c95bd1fde81acc963
brandontaylor1994 <brandontaylor1994@gmail.com>
2016-04-20 15:41:17 -0700

    feature-1: Removed unnecessary function used during trial phase

--- server/cardreader/views.py
+++ server/cardreader/views.py
@@ -24,21 +24,3 @@ def index(request):
     else:
         form = NameForm()
     return render(request, 'cardreader/index.html', {'form': form, 'name': name})
-
-# def get_name(request):
-#     name = "default"
-#     if request.method == 'POST':
-#         prev_form = NameForm(request.POST)
-#         form = NameForm()
-#
-#         name = request.POST['w_number']
-#         pattern = re.compile("(W[0-9]{8})")
-#         match = pattern.search(name)
-#         name = match.group(1)
-#
-#         print('Just set name: ' + match.group(1))
-#
-#     else:
-#         form = NameForm()
-#
-#     return render(request, 'cardreader/name.html', {'form': form, 'name': name})
