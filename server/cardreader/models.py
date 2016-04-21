b8cb3a581f876e4c498098d5de55d5dd2ffab1bd
brandontaylor1994 <brandontaylor1994@gmail.com>
2016-04-20 17:45:45 -0700

    feature-2: Added a student table with three fields to model.py

--- server/cardreader/models.py
+++ server/cardreader/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# Create a Student table in the database with the following fields
+class Student(models.Model):
+    w_num = models.CharField(max_length=9)
+    in_time = models.TimeField()
+    out_time = models.TimeField()
