8d212c6b4c2ec0702f8035ada2367115a66f4b3f
Josh Richmond <josh.richmond@gmail.com>
2016-04-24 16:01:07 -0400

    Added superuser, changed student display type to W-number, added date to student table

--- server/cardreader/models.py
+++ server/cardreader/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 # Create a Student table in the database with the following fields
 class Student(models.Model):
-    w_num = models.CharField(max_length=9)
-    in_time = models.TimeField()
-    out_time = models.TimeField(null=True)
+	def __unicode__(self):
+		return format(self.w_num) + '-' + format(self.date)
+	w_num = models.CharField(max_length=9)
+	in_time = models.TimeField()
+	out_time = models.TimeField(null=True)
+	date = models.DateField(auto_now_add=True)
