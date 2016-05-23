a10ba1bb089c8406e5c481382eb211974dd1db77
euranium <mountrainier13@gmail.com>
2016-05-23 13:59:00 -0700

    updated front end to look good

--- server/cardreader/forms.py
+++ server/cardreader/forms.py
@@ -4,6 +4,6 @@ from django import forms
 # Creates a new form with the following field and attributes.
 # Each time the class is instantiated it constructs a new html <form>
 class NameForm(forms.Form):
-    w_number = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': '########', 'autofocus': 'autofocus'}), label='W   ', max_length=100)
+    attrs={'placeholder': '########', 'autofocus': 'autofocus', 'class': 'form-control', 'id': 'num'}
+    w_number = forms.CharField(widget=forms.TextInput(attrs), label='W   ', max_length=100)
 
