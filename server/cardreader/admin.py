d9f7ed058c75225d0f31b127b2d6589c0a1e3952
Josh Richmond <josh.richmond@gmail.com>
2016-05-26 12:21:08 -0400

    added readability to students section of the admin page aswell as search function

--- server/cardreader/admin.py
+++ server/cardreader/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from cardreader.models import Student
 
-from .models import Student
+def sort_st(modeladmin):
+	str("test")
+sort_st.short_description = "test"
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	readonly_fields = ('date',)
+	list_display = ('w_num','date','in_time','out_time',)
+	ordering = ('date',)
+	actions = [sort_st]
+	search_fields = ('w_num','date',)
+	list_filter = ('date',)
+admin.site.register(Student,StudentAdmin)
 # Register your models here.
+
