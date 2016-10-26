7e9fec5c64b5be8a9e838b56c350c63406c58dee
Brandon Taylor <brandontaylor1994@gmail.com>
2016-10-26 16:19:45 -0700

    removed virtual environment and committed new migration

--- virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/harveyr8/Documents/cs/project/49X-DRS-Checking/virtualenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
