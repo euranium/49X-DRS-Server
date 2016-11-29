f7b966de9310a268d83da4da9720c5d96cef9d39
Jack Burke <burkej27@LabImage2016-17>
2016-11-28 18:12:05 -0800

    Refactored logging to enable rapid swipe error

--- server/cardreader/views.py
+++ server/cardreader/views.py
@@ -15,35 +15,45 @@ def index(request):
 
         # Use regex to parse out w_number
         w_num = request.POST['w_number']
-        pattern = re.compile("W?([0-9]{8})")  # Possibly always add the W, or make sure it doesnt add any numbers after it, for using the card
-        match = pattern.search(w_num)
-
+        match = isValidId(w_num);
         if not match: #if it does not match the pattern, give error
             message = "Invalid number, please enter your 8 digit W#"
             messages.success(request, message)
-        # w_num = match.group(1)
         else :
             w_num = match.group(1)
-            query = Student.objects.filter(w_num=w_num).filter(out_time=None).last()
-            if query:  # log user out
-                current_time = datetime.datetime.now()
-                query.out_time = current_time
-                query.save()
-                # Compile message to be sent to web page
-                message = "Thank you for logging out, " + w_num
-                messages.success(request, message)
-            else:
-                # Create a new instance of Student class and log student in
-                current_time = datetime.datetime.now()
-                student = Student(w_num=w_num, in_time=current_time)
-                student.save()
-                # Compile message to be sent to web page
-                message = "Thank you for logging in, " + w_num
-                messages.success(request, message)
-
+            message = processUserLogging(w_num)
+            messages.success(request, message)
         # Redirect the web page to the same path but with the form now empty POST -> GET (avoid resubmission scenario)
             return HttpResponseRedirect(request.path)
     else:
         # Create a new form and pass it to the web page
         form = NameForm()
     return render(request, 'cardreader/index.html', {'form': form, 'w_num': w_num})
+
+def isValidId(w_num):
+    pattern = re.compile("W?([0-9]{8})")  # Possibly always add the W, or make sure it doesnt add any numbers after it, for using the card
+    match = pattern.search(w_num)
+    return match;
+
+def processUserLogging(w_num):
+    query = Student.objects.filter(w_num=w_num).filter(out_time=None).last()
+    current_time = datetime.datetime.now()
+    if query:
+        if ((current_time - datetime.timedelta(minutes=1)).time() < query.in_time):
+            #User tried to swipe again within 60 seconds
+            #Most likely accidental multiple swipe, ignore
+            message = "You are already logged in " + w_num + " wait 60 seconds before logging out"
+        else:
+            query.out_time = current_time
+            query.save()
+            # Compile message to be sent to web page
+            message = "Thank you for logging out, " + w_num
+    else:
+        # Create a new instance of Student class and log student in
+        current_time = datetime.datetime.now()
+        student = Student(w_num=w_num, in_time=current_time)
+        student.save()
+        # Compile message to be sent to web page
+        message = "Thank you for logging in, " + w_num
+
+    return message
