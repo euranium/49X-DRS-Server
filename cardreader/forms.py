a5c93de0ce519c5ec7af72e863f0b6f58a7adfb8
Garrett Gregory <gregorg@LabImage2016-17>
2016-11-30 12:17:15 -0800

    removed the dirty colon

--- cardreader/forms.py
+++ cardreader/forms.py
@@ -5,5 +5,4 @@ from django import forms
 # Each time the class is instantiated it constructs a new html <form>
 class NameForm(forms.Form):
     attrs={'placeholder': '########', 'autofocus': 'autofocus', 'class': 'form-control', 'id': 'num'}
-    w_number = forms.CharField(widget=forms.TextInput(attrs), label='W   ', max_length=100)
-
+    w_number = forms.CharField(widget=forms.TextInput(attrs), label='W   ', label_suffix='', max_length=100)
