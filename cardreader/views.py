b7b5d32513f1a609e3880cdb14c57387a9f8cf9c
Garrett <gj.gregory@gmail.com>
2016-12-08 15:36:19 -0800

    changed time to 6 hours 30 mins. we weren't taking date into account before. fixed.

--- cardreader/views.py
+++ cardreader/views.py
@@ -40,13 +40,14 @@ def isValidId(w_num):
 def processUserLogging(w_num):
     query = Student.objects.filter(w_num=w_num).filter(out_time=None).last()
     current_time = timezone.now()
+    current_date = datetime.datetime.now().date()
     if query:
         dur = (current_time - query.in_time).seconds
-        if (dur / 60 < 1):
+        if (dur / 60 < 1 and query.in_time.date() == current_date):
             # User tried to swipe again within 60 seconds
             # Most likely accidental multiple swipe, ignore
             message = "You are already logged in " + w_num + " wait " + str(60-dur) + " seconds before logging out"
-        elif (dur / 60 <= 300):
+        elif (dur / 60 <= 390 and query.in_time.date() == current_date): # 390 == 6 hours 30 mins
             # User properly logged out
             in_time = query.in_time
             query.logged_out = True
